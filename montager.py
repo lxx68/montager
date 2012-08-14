4f045444d648525995ade3aecf722f0d6777b780
Jaakko Korhonen <luxor68@gmail.com>
2012-08-14 14:27:33 +0300

    Added comments.

--- montager.py
+++ montager.py
@@ -1,34 +1,71 @@
 #!/usr/bin/python
+""" monteger.py
+Creates montage images of every 10 seconds from the selected video file.
+Images are named as 'montage-...' and placed into same folder with the video.
+Usage: python montager.py file
+file: video file
 
+Examples: 
+montager.py poo.avi
+montager.py /usr/poo/poo.mp4
+"""
 import sys
 
 class mediaI:
+    """
+    This class determines and stores the following metadata: aspect ratio and runtime. 
+    """
     def __init__(self, path):
         xml = self.read_mediainfo(path)
         self.fill_variables(xml)
         pass
 
     def read_mediainfo(self, path):
+        """
+        Gets videofiles metadata in XML -format.
+        Uses mediainfo.
+        """
         pass
 
     def fill_variables(self, xml):
+        """
+        Fills the member variables based on the XML.
+        Uses Elementtree- package.
+        """
         pass
 
 # the main program
 
 def extract_images(path):
+    """
+    Extracts a frame from the video file every 10 seconds.
+    Uses ffmpeg.
+    """
     pass
 
 def create_montages(path):
+    """
+    Uses ImageMagick's montage -tool.
+    """
     pass
 
 def create_header(path, mi):
+    """
+    Uses the Python Imaging Library. (PIL)
+    """
     pass
 
 def join_images(path):
+    """
+    Attaches the headerimage on top of the montages.
+    Uses PIL.
+    """
     pass
 
 def cleanup(path):
+    """
+    Deletes temporaty files.
+    """
     pass
 
 def main():
